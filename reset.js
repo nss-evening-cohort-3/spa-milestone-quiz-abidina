baf5c7444092ac38df4aead8e926556f72fe06f5
Abbie Holder <abigail.c.holder@gmail.com>
2016-04-11 19:54:44 -0500

    tweaked button, realized I did a thing

--- reset.js
+++ reset.js
@@ -6,17 +6,22 @@ var CarLot = (function (anything) {
   anything.resetCard = function() {
     btn.addEventListener('click', function(e){
       event.preventDefault();
-      var clickedCards = document.getElementsByClassName('clicked');
-      clickedCards[0].classList.remove('clicked');
+      let clickedCard = document.getElementsByClassName('clicked');
+      clickedCard[0].classList.remove('clicked'); // have to access it with index bc "getelementsbyclassname" creates an array
     })
   };
 
   CarLot.resetCard();
 
-  
-  // anything.changeCard = function(clickedCar, color) {
 
-  // };
+  anything.changeCard = function(clickedCar, color) {
+    let clickedCard = document.getElementsByClassName('clicked');
+
+  };
+
+  //The other function changes the thickness of the border of a car element, and changes its background color. The function must accept two arguments.
+    // 1. A car DOM element that was clicked on.
+    // 1. A color name.
 
   return anything;
 }( CarLot || {} ));
