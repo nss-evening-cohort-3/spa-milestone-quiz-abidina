73a515ff7cb8c68aa3bb3dfdd504ccc0aa1f7aeb
Abbie Holder <abigail.c.holder@gmail.com>
2016-05-07 14:10:20 -0500

    functioning bootstrap rows

--- quiz.js
+++ quiz.js
@@ -4,17 +4,16 @@ var CarLot = (function (anything) {
   anything.populatePage = function(carInventory) {
   var carEl = document.getElementById('carElement');
   carEl.innerHTML = "";
-  // var carInventory = CarLot.getInventory();
+  var string = "";
 
   // Loop over the inventory and populate the page
   for (let i = 0; i < carInventory.cars.length; i++) {
     var currentCar = carInventory.cars[i];
-    let carCard = buildCard(currentCar, i);
-    let cardDiv = document.createElement('div');
-    cardDiv.innerHTML = (carCard);
-    carEl.appendChild(cardDiv); //adds div to dom while maintaining event listeners
+    string += buildCard(currentCar, i);
     let card = document.getElementById('card--${index}');
   }
+
+  carEl.innerHTML = string;
   
 CarLot.activateEvents();
 
@@ -22,14 +21,16 @@ CarLot.activateEvents();
 function buildCard (currentCar, i) {
     var fillCard = "";
 
+    if (i % 3 === 0) {
+      fillCard += '<div class="row-fluid">';
+    }
     // create individual ids in DOM for articles
-    fillCard += '<div class="row-fluid">';
     fillCard += '<article class="carCards col-md-4" id="currentCard' + i + '" style="border: 2px solid ' + currentCar.color + '"><h3>' + currentCar.year + ' ' +  currentCar.make + ' ' + currentCar.model + '</h3><section><p>' + currentCar.color + '</p><p>$' + currentCar.price + '</p></section><section class="description"><p id="abtCar">' + currentCar.description + '</p></section><footer>' + "Available" + '</footer></article>';
-    fillCard += '</div>';
-
+    if ((i + 1) % 3 === 0) {
+      fillCard += '</div>';
+    }
     return fillCard;
     }
-
 }
 
 
